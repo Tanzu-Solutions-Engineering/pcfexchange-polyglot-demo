146e4392b4d7632d54ce0c7dad93ea6aee170b1b
Andrew Stakhov <astakhov@pivotal.io>
2017-10-25 22:12:46 -0400

    Hystrix

--- Complete/order-manager/src/main/java/io/pivotal/om/controller/UIServices.java
+++ Complete/order-manager/src/main/java/io/pivotal/om/controller/UIServices.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import io.pivotal.om.domain.ExchangeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.*;
@@ -23,35 +25,26 @@ import io.pivotal.om.repository.OrderRepository;
 @RestController
 @Configuration
 @EnableAutoConfiguration
+
 public class UIServices {
 	
 	Logger logger = LoggerFactory.getLogger(UIServices.class);
-	
+
 	private OrderRepository or;
-	private RestTemplate restTemplate;
 	private DiscoveryClient discoveryClient;
-
-	@Value("${config.rate}")
-	int rate;
-
+	private ExchangeService exchangeService;
 
 	@Autowired
-	public UIServices(OrderRepository or, RestTemplate restTemplate, DiscoveryClient discoveryClient) {
+	public UIServices(OrderRepository or, DiscoveryClient discoveryClient, ExchangeService exchangeService) {
 		this.or = or;
-		this.restTemplate = restTemplate;
 		this.discoveryClient = discoveryClient;
+
+		this.exchangeService = exchangeService;
 	}
 
 	@DeleteMapping(value="api/client/{clientId}/order/{orderId}")
 	public ExecutionReport deleteOrder(@PathVariable String clientId, @PathVariable String orderId) {
-		ExecutionReport order = or.findOne(orderId);
-		String symbol = order.getSymbol();
-		String url = lookupUrlForExchange(symbol) + "/api/order/" + String.valueOf(orderId);
-		ResponseEntity<ExecutionReport> re = restTemplate.exchange(url, HttpMethod.DELETE, null, ExecutionReport.class);
-		ExecutionReport eor = re.getBody();
-		or.save(eor);
-		or.flush();
-		return eor;
+		return exchangeService.deleteOrder(clientId, orderId);
 	}
 
 	@RequestMapping(value="api/client/{clientId}/orders", method=RequestMethod.GET)
@@ -75,41 +68,10 @@ public class UIServices {
 
 
 	@PostMapping(value="api/order")
-	@Transactional
 	@ResponseBody
 	public ExecutionReport placeOrder(@RequestBody ExecutionReport clientOrderRequest) {
-		String orderId = java.util.UUID.randomUUID().toString();
-		clientOrderRequest.setOrderId(orderId);
-		logger.debug("Created new order with ID=" + orderId);
-		String url = lookupUrlForExchange(clientOrderRequest.getSymbol()) + "/api/order/" + String.valueOf(orderId);
-		logger.debug("Exchange service URL=" + url);
-		
-	    HttpHeaders headers = new HttpHeaders();
-	    headers.setContentType(MediaType.APPLICATION_JSON);
-		HttpEntity<ExecutionReport> httpOrderRequest = new HttpEntity<>(clientOrderRequest, headers);
-		ResponseEntity<ExecutionReport[]> re = restTemplate.exchange(url, HttpMethod.PUT, httpOrderRequest, ExecutionReport[].class);
-		
-		ExecutionReport[] eor = re.getBody();
-		HashMap<String,ExecutionReport> ordersToSave = new HashMap<>();
-
-		for(ExecutionReport er : eor)
-		{
-			er.setLastCommission(rate);
-			ordersToSave.put(er.getOrderId(), er);
-		}
-		ExecutionReport newOrderLastState = ordersToSave.get(orderId);
-		newOrderLastState.setNew(true);
-		ordersToSave.forEach((l, order) -> or.save(order));
-		or.flush();
-
-		return newOrderLastState;
+		return exchangeService.placeOrder(clientOrderRequest);
 	}
 
-	  private String lookupUrlForExchange(String symbol) {
-//		  return "http://exchange-btcusd.apps.pcf.guru";
-		  List<ServiceInstance> serviceInstances = discoveryClient.getInstances("Exchange_" + symbol);
-		  String url = serviceInstances.get(0).getUri().toString();
-		  return url;
-	  }
-		
+
 }
