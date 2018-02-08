990d2da0e103542e54d1f447a1f643d1a9faa287
Andrew Stakhov <astakhov@pivotal.io>
2018-02-08 12:43:17 -0500

    Legacy exchange

---
+++ Complete/ExchangeLegacy/Controllers/OrderController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Almirex.Contracts.Fields;
+using Almirex.Contracts.Messages;
+using ExchangeLegacy.Services;
+
+namespace ExchangeLegacy.Controllers
+{
+    public class OrderController : ApiController
+    {
+        private readonly OrderbookService _orderbookService;
+
+
+        // GET api/values
+        public OrderController(OrderbookService orderbookService)
+        {
+            _orderbookService = orderbookService;
+        }
+
+        [HttpGet]
+        public IEnumerable<ExecutionReport> Get()
+        {
+            return _orderbookService.OrderBook
+                .Asks
+                .Union(_orderbookService.OrderBook.Bids)
+                .Select(x => x.ToExecutionReport(ExecType.OrderStatus))
+                .ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet]
+        public IHttpActionResult Get(string id)
+        {
+            var order = _orderbookService.OrderBook.FindOrder(id);
+            if (order != null)
+                return Json(order.ToExecutionReport(ExecType.OrderStatus));
+            return NotFound();
+        }
+
+
+        // PUT api/values/5
+        [HttpPut]
+        public List<ExecutionReport> Put(string id, [FromBody]ExecutionReport order)
+        {
+            var results = _orderbookService.NewOrder(order);
+            return results;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        public IHttpActionResult Delete(string id)
+        {
+            var executionReport = _orderbookService.CancelOrder(id).FirstOrDefault();
+            return Json(executionReport);
+        }
+    }
+}
---
+++ Complete/ExchangeLegacy/Global.asax.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Almirex.Contracts.Interfaces;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Autofac.Integration.Web;
+using Autofac.Integration.WebApi;
+using ExchangeLegacy.Config;
+using ExchangeLegacy.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using Pivotal.Discovery.Client;
+using Pivotal.Extensions.Configuration;
+using Pivotal.Extensions.Configuration.ConfigServer;
+using Steeltoe.CircuitBreaker.Hystrix;
+using Steeltoe.CloudFoundry.Connector.MySql;
+using Steeltoe.CloudFoundry.Connector.Rabbit;
+
+namespace ExchangeLegacy
+{
+    public class WebApiApplication : System.Web.HttpApplication, IContainerProviderAccessor
+    {
+        private static IContainerProvider _containerProvider;
+
+        public IContainerProvider ContainerProvider => _containerProvider;
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+            var services = new ServiceCollection();
+
+
+            ILoggerFactory logFactory = new LoggerFactory();
+            logFactory.AddConsole(minLevel: LogLevel.Debug);
+            string env = "Production";
+            ServerConfig.RegisterConfig(configBuilder =>
+                configBuilder
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+                    .AddJsonFile($"appsettings.{env}.json", optional: true)
+                    .AddConfigServer(env, logFactory)
+                    .AddEnvironmentVariables());
+//            services.AddDiscoveryClient(ServerConfig.Configuration);
+            services.AddOptions();
+            services.AddMySqlConnection(ServerConfig.Configuration);
+            services.AddSingleton<OrderbookService>();
+            services.Configure<Steeltoe.Discovery.Client.SpringConfig>(ServerConfig.Configuration.GetSection("spring"));
+            services.Configure<ExchangeConfig>(ServerConfig.Configuration.GetSection("config"));
+            services.AddSingleton<IFeeSchedule, FlatFeeScheduler>();
+            services.AddRabbitConnection(ServerConfig.Configuration, ServiceLifetime.Singleton);
+//            services.AddHystrixMetricsStream(ServerConfig.Configuration);
+
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            
+            builder.Register(x => x.Resolve<MySqlConnection>()).As<IDbConnection>();
+            builder.Register(ctx => logFactory).As<ILoggerFactory>().SingleInstance();
+            builder.RegisterGeneric(typeof(Logger<>)).As(typeof(ILogger<>)).SingleInstance();
+            builder.RegisterDiscoveryClient(ServerConfig.Configuration);
+            builder.RegisterHystrixMetricsStream(ServerConfig.Configuration);
+            var config = GlobalConfiguration.Configuration;
+            // Register your Web API controllers.
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // OPTIONAL: Register the Autofac filter provider.
+            builder.RegisterWebApiFilterProvider(config);
+
+            // OPTIONAL: Register the Autofac model binder provider.
+            builder.RegisterWebApiModelBinderProvider();
+            var container = builder.Build();
+            container.StartDiscoveryClient();
+            container.StartHystrixMetricsStream();
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+            // ensure that discovery client component starts up
+//            container.Resolve<IDiscoveryClient>();
+            container.Resolve<OrderbookService>().Recover();
+            _containerProvider = new ContainerProvider(container);
+            Console.WriteLine(">> Exchange Started<<");
+            
+            //            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
